5d1309a392bcd61ae2c7d34bb4e31eb5e02c1899
liubamiron <liuba.miron@gmail.com>
2022-03-01 15:22:18 +0200

    format

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -2,29 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Imobil;
+use Illuminate\Http\Request;
 use App\Services\ModelLogger;
 
-class HouseController extends Controller{
-    public function index() { 
-        $request = request()->all(); 
+class HouseController extends Controller
+{
+    public function index()
+    {
+        $request = request()->all();
 
         $roomsnr = $request['rooms'] ?? '';
-        $imobils;  
+        $imobils;
 
-        if($roomsnr === ''){
+        if ($roomsnr === '') {
             $imobils = Imobil::select()->where('type', '=', 'HOUSE')->paginate(7);
-        }
-        else{
-            $imobils = Imobil::select()->where('rooms_nr', '=', $roomsnr) 
-                                        ->where( 'type', '=', 'HOUSE')->paginate(7);
+        } else {
+            $imobils = Imobil::select()->where('rooms_nr', '=', $roomsnr)
+                ->where('type', '=', 'HOUSE')->paginate(7);
         }
 
         $rooms = Imobil::select('rooms_nr')->distinct()->get();
 
-        return view('imobils.imobil', ['imobils' => $imobils, 'rooms' => $rooms, 'roomsnr' => $roomsnr]);     
+        return view('imobils.imobil', ['imobils' => $imobils, 'rooms' => $rooms, 'roomsnr' => $roomsnr]);
     }
-       
-}
 
+}
