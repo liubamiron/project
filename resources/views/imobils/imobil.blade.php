f80f024b1b25dbbfd82fea621cb8861e4ed5375a
liubamiron <liuba.miron@gmail.com>
2022-03-01 16:21:56 +0200

    add request::path()

--- resources/views/imobils/imobil.blade.php
+++ resources/views/imobils/imobil.blade.php
@@ -1,10 +1,15 @@
 @extends('layout')
 @section('content')
 
-<div class="row">
-    <form method="GET" action="/apartment" class="row row-cols-3 mb-4">
+<div class="row"> 
+
+
+        <form method="GET" action='/{{  Request::path() }}' class="row row-cols-3 mb-4">
+        <div class="col">
+            <h5>Sort  by rooms nr</h5>
+        </div>
          <div class="col">
-               <select class="form-select" name="rooms">
+            <select class="form-select" name="rooms">
                @foreach($rooms  as $room) 
                     <option value = {{$room->rooms_nr}}
                         @if((int)$roomsnr === (int)$room->rooms_nr) selected @endif>         
@@ -12,29 +17,12 @@
                @endforeach
             </select>
        </div> 
+       
            <div class="col">
                <button class="btn btn-primary">Filter</button>
            </div>
    </form>         
-</div>
-
-<div class="row">
-    <form method="GET" action="/apartment" class="row row-cols-3 mb-4">
-            <div class="col">
-               <select class="form-select" name="city">
-                    @foreach($cities  as $city) 
-                        <option value={{$city->city}}
-                          @if($filter['city'] === $city->city) selected @endif>                      
-                        {{$city->city}}
-                        </option>
-                    @endforeach
-                </select>
-            </div> 
-            <div class="col">
-                <button class="btn btn-primary">Filter</button>
-            </div>
-    </form>
-</div>  
+</div> 
 
 
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
