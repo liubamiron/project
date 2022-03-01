421209eb3a2fa9622059970fed7bfc8b0a2ed11a
liubamiron <liuba.miron@gmail.com>
2022-03-01 16:32:35 +0200

    change rows

--- resources/views/contacts/contactUs.blade.php
+++ resources/views/contacts/contactUs.blade.php
@@ -29,31 +29,31 @@
                         <input type="email" value="{{old('email')}}" required="required" class="form-control" name="email" id="email" placeholder="Email">
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="name">Nume</label>
-                        <input type="text" value="{{old('name')}}" required="required" class="form-control" name="name" id="name" placeholder="Nume">
+                        <label for="name">Name</label>
+                        <input type="text" value="{{old('name')}}" required="required" class="form-control" name="name" id="name" placeholder="Name">
                         
                     </div>
                     <div class="form-group col-md-6">
                         <label for="inputState">Departament</label>
                         <select id="department" class="form-control" name="department">
                             <option value="">Selecteaza un departament</option>
-                            <option @if(old('department') === 'administration') selected @endif value="administration" >Administratie</option>
-                            <option @if(old('department') === 'accounting') selected @endif value="accounting" >Contabilitate</option>
-                            <option @if(old('department') === 'technicalDepartment') selected @endif value="technicalDepartment" >Departamentul tehnic</option>
-                            <option @if(old('department') === 'logistic') selected @endif value="logistic" >Logistica</option>
+                            <option @if(old('department') === 'accounting') selected @endif value="accounting" >Accounting</option>
+                            <option @if(old('department') === 'administration') selected @endif value="administration" >Administration</option>
+                            <option @if(old('department') === 'technicalDepartment') selected @endif value="technicalDepartment" >Department</option>
+                            <option @if(old('department') === 'estate') selected @endif value="estate" >Real Estate</option>
                         </select>
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="inputState">Raioane</label>
+                        <label for="inputState">Districts</label>
                         <select id="districts" class="form-control" name="districts[]" multiple>
-                            <option value="">Selecteaza un raion</option>
+                            <option value="">Select one district</option>
                             <option @if(in_array('chisinau', old('districts', []))) selected @endif value="chisinau" >Mun. Chisinau</option>
                             <option @if(in_array('orhei', old('districts', []))) selected @endif value="orhei" >Orhei</option>
                             <option @if(in_array('straseni', old('districts', []))) selected @endif value="straseni" >Straseni</option>
                         </select>
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="name">Mesaj</label>
+                        <label for="name">Message</label>
                         {{-- fara spatii --}}
                         <textarea class="form-control" name="message" id="message" cols="30" rows="10">{{old('message') }} </textarea>
                     </div>
@@ -62,12 +62,12 @@
                     <div class="form-check">
                         <input class="form-check-input" @if(old('readTerms')) checked @endif name="readTerms" type="checkbox" id="gridCheck" value="1">
                         <label class="form-check-label" for="gridCheck">
-                            Am citit regulile
+                            I have read and understood
                         </label>
                     </div>
                 </div>
                 @csrf
-                <button type="submit" class="btn btn-primary">Trimite mesaj</button>
+                <button type="submit" class="btn btn-primary">Send message</button>
             </form>
         </div>
     </div>
