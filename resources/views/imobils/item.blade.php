e749e8992edd6a7e2c0cd631307ce5af7628965c
liubamiron <liuba.miron@gmail.com>
2022-03-01 16:25:30 +0200

    change view in to price button

--- resources/views/imobils/item.blade.php
+++ resources/views/imobils/item.blade.php
@@ -1,31 +1,31 @@
  
-    <div class="card">
+    <div class="card" >
 
-           {{-- <img src="{{ asset('/assets/images/studio.jpg/') }}"  alt="{{ $imobil ->type }}">  --}}
-           {{-- <img src="{{ asset('/assets/images/image' .id. '.jpg') }}">  --}}
+{{-- <img src="{{ asset('/assets/images/studio.jpg/') }}"  alt="{{ $imobil ->type }}">  --}}
+{{-- <img src="{{ asset('/assets/images/image' .id. '.jpg') }}">  --}}
 
-    <img src="{{ asset('/assets/images/image' .$imobil->id. '.jpg') }}"> 
+<img src="{{ asset('/assets/images/image' .$imobil->id. '.jpg') }}" alt="{{ $imobil->type }}" class="d-block w-100" style="width:100%"> 
 
-          {{-- <h4>  {{$path1}}</h4> --}}
-            <div class="card-body">
-              <p class="card-text">{{ $imobil->id }}</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <div class="btn-group"> 
-                <a href="{{route('itemById', ['id' => $imobil->id]) }}"
-                  class="btn btn-sm btn-outline-secondary">
-                  View
-                </a>
+ <div class="card-body">
+   
+   <div class="d-flex justify-content-between align-items-center">
+     <div class="btn-group">
+       <div class="btn-group">
+     <a href="{{route('itemById', ['id' => $imobil->id]) }}"
+       class="btn btn-sm btn-outline-primary">
+       {{ $imobil->price }}/month
+     </a>
+     
 
-                </div>
-                <small class="text-muted">{{ $imobil->created_at }}</small>
-              </div>
-            </div>
-          </div>
-
-      
-    </div>
+     </div>
+     <br>
+     {{-- <small class="text-muted">{{ $imobil->created_at }}</small> --}}
+   </div>
+ </div>
+</div>
 
 
+</div>
 
-          
+
+
