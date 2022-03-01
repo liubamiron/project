c960aa2a70979be154726167988d8eb15818aa3f
liubamiron <liuba.miron@gmail.com>
2022-03-01 15:02:35 +0200

    delete

--- app/Console/Commands/Game2.php
+++ app/Console/Commands/Game2.php
@@ -17,7 +17,7 @@ class Game2 extends Command
      *
      * @var string
      */
-    protected $description = 'Play russian roulette game.';
+    protected $description = 'russian roulette';
     private CacheRepository $cacheRepository;
     /**
      * Create a new command instance.
@@ -36,79 +36,5 @@ class Game2 extends Command
      */
     public function handle()
     {
-        $name = $this->ask(
-            "Only the brave will play this game. Please, name yourself !"
-        );
-        do {
-            $bulletInChamber = rand(1,6);
-            $playerGotChamber = rand(1,6);
-            $this->info("Bullet in chamber: " . $bulletInChamber);
-            $this->info("Player got chamber: " . $playerGotChamber);
-
-            if($bulletInChamber === $playerGotChamber)
-            {
-                $cursed_numbers = $this->cacheRepository->get('Cursed-numbers', []);
-                $cursed_numbers[$bulletInChamber] = $cursed_numbers[$bulletInChamber] ?? 0;
-                $cursed_numbers[$bulletInChamber]++;
-
-                $this->cacheRepository->set('Cursed-numbers', $cursed_numbers, 86400);
-
-
-                $this->info("Congrats! Back to zero leader score. Reason: You died !");
-                $statistics = $this->cacheRepository->get('Roulette results', []);
-                $statistics[$name] = 0;
-                $this->cacheRepository->set('Roulette results', $statistics, 86400);
-                $killstreak = $this->cacheRepository->get('Kill-streak', 0);
-                $killstreak++;
-                $this->cacheRepository->set('Kill-streak', $killstreak, 86400);
-                switch($killstreak){
-                    case 1:
-                        break;
-                    case 2:
-                        $this->info("DOUBLE KILL!");
-                        break;
-                    case 3:
-                        $this->info("TRIPLE KILL!");
-                        break;
-                    case 4:
-                        $this->info("MULTI KILL!");
-                        break;
-                    case 5:
-                        $this->info("M-M-MONSTER KILL!");
-                        break;
-                    default:
-                        $this->info("DOMINATING!");
-                }
-            } else {
-                $statistics = $this->cacheRepository->get('Roulette results', []);
-                $statistics[$name] = $statistics[$name] ?? 0;
-                    $this->info("You survived !");
-                    $statistics[$name]++;
-                $this->cacheRepository->set('Roulette results', $statistics, 86400);
-                $this->cacheRepository->set('Kill-streak', 0, 86400);
-            }
-        } while($bulletInChamber !== $playerGotChamber && strtolower($this->ask("Type Y to play again ?")) === 'y');
-
-        $statistics = $this->cacheRepository->get('Roulette results', []);
-        $table = [];
-        foreach ($statistics as $key => $count) {
-            $table[] = [$key, $count];
-        }
-        ksort($table);
-
-        $this->table(['Name', 'Times survived'], $table);
-
-        $cursed_numbers = $this->cacheRepository->get('Cursed-numbers', []);
-
-        $cursedTable = [];
-
-        foreach ($cursed_numbers as $key => $count) {
-            $cursedTable[] = [$key, $count];
-        }
-        sort($cursedTable);
-
-        $this->table(['Cursed Number', 'Times died'], $cursedTable);
-
-        $this->info("Gun's kill streak: " . $this->cacheRepository->get('Kill-streak',0));
     }
 }
