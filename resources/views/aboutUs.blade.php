566cbcf1f4bf17e24b932f395f71c5ee3e0501ca
liubamiron <liuba.miron@gmail.com>
2022-03-01 16:14:19 +0200

    aboutus page

---
+++ resources/views/aboutUs.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+
+
+<head>
+
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="author" content="LM"><!-- my name1 -->
+
+	<title>AllRent</title>
+
+
+	<!-- <link rel="shortcut icon" type="image/x-icon" href="images/icons/building.png"> -->
+	
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+	<!-- <script src="js/script.js" type="text/javascript"></script> -->
+	
+	<!--our styles -->
+	
+	<link rel="stylesheet" href="{{ asset('/assets/css/app.css') }}">
+
+</head>
+
+
+
+<body>
+	<!-- <img src="{{ asset('/assets/images/studio.jpg') }}"> -->
+<div class="container">
+	<!--header-->
+@include('header')
+            <div class="row">
+                <div class="col-sm-12">
+<br>
+<br>
+
+        <div class="container-fluid vh-100" >
+            <div class="" style="margin-top:10px">
+                <div class="rounded d-flex justify-content-center">
+                    <div class="col-md-12 col-sm-12 shadow-lg p-5 bg-light">
+                        <div class="text-center">
+                            <h3 class="text-primary">About Us </h3>
+                        </div>
+                        <div class="p-4">
+                            <p> At AllRent you can find the best properties for rent.</p>
+<p>Our experience in Chisinau short term rental and long term rental is already vast and we remark that our new customers have a series of recurring questions.</p>
+<p>The short term rent in Chisinau is a cost-effective alternative to a hotel stay. An apartment can be rented from 2 nights up to several weeks.
+     The reason why this is such a widely popular option all over the world is due to a well developed and established service. </p>
+     <p>Renting a short term apartment in Chisinau is a very safe accommodation option. All of our apartments are located in the center of Chisinau, which is perfectly safe.</p>
+     <p> Serviced Chisinau apartments are quite busy and therefore it is better not to count on an early Check-In. If you want to be assured of an early check in or late check out – you should reserve and pay the apartment for ½ day more, each case is individual.</p>
+                        </div>
+                        <hr>
+                            <br>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+</div>
+</div>
+
+</div>
+
+<!-- </div> -->
+
+	<!-- ========================= SECTION CONTENT END// ========================= -->
+	<!-- ========================= FOOTER ========================= -->
+	{{-- @include('footer') --}}
+	
+</body>
+</html>
